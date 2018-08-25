8bd91bf698011800f0cacd33142f2a61618b656f
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2018-08-24 18:23:56 -0700

    first commit

---
+++ turtle_circle.py
@@ -0,0 +1,7 @@
+import turtle # allows us to use the turtles library
+wn = turtle.Screen() # creates a graphics window
+grace = turtle.Turtle() # create a turtle named grace grace.penup()
+grace.penup()
+grace.goto(0, -200)
+grace.pendown()
+grace.circle(200)
---
+++ turtle_smiley_face.py
@@ -0,0 +1,34 @@
+import turtle               # allows us to use the turtles library
+wn = turtle.Screen()        # creates a graphics window
+grace = turtle.Turtle()      # create a turtle named alex
+#grace.forward(150)           # tell alex to move forward by 150 units
+#grace.left(90)               # turn by 90 degrees
+#grace.forward(75)            # complete the second side of a rectangle
+grace.circle(70)
+grace.penup()
+grace.left(90)
+grace.forward(55)
+grace.pendown()
+grace.forward(40)
+grace.penup()
+grace.left(90)
+grace.forward(30)
+grace.pendown()
+grace.circle(10)
+grace.penup()
+grace.right(180)
+grace.forward(51)
+grace.right(90)
+grace.forward(8)
+grace.pendown()
+grace.circle(10)
+grace.penup()
+grace.forward(50)
+grace.right(90)
+grace.pendown()
+grace.forward(40)
+grace.penup()
+grace.forward(300)
+
+
+
