1116ad6eadf9ea9af8a77b121242eace891e4657
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2019-01-06 15:52:29 -0600

    print and return string reversed

---
+++ reverse_string.py
@@ -0,0 +1,17 @@
+def print_str_reversed(stri):
+	"""this function copys a string then steps thru it backwards by 1 printing it reversed"""
+	print(stri[::-1])
+
+
+print_str_reversed("Grace")
+
+
+
+def returns_reversed_str(strings):
+	"""this function copys a string then steps thru it backwards by 1 returning it reversed"""
+
+	return strings[::-1]
+
+
+
+print(returns_reversed_str("Grace"))
