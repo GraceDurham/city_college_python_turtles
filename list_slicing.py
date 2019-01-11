e0cf2ad51103d25e50b7f23e70b7e4dfcb4a6c86
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2019-01-11 14:04:36 -0600

    first commit

---
+++ list_slicing.py
@@ -0,0 +1,13 @@
+a_list = ["a", "b", "c", "d", "e", "f"]
+print(a_list[1:3]) #bc
+print(a_list[:4]) #abcd
+print(a_list[3:]) #def
+print(a_list[:]) #abcdef
+
+
+#what is printed by the following statements?
+
+alist =[3, 67, "cat", [56, 57,"dog"], [], 3.14, False]
+print(alist[4:])
+
+#answer is [[], 3.14, False]
