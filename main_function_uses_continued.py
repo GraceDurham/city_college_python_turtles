7292306279c91b40293d7db87fdea77e0c963227
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2019-01-14 18:22:12 -0600

    first commit

---
+++ main_function_uses_continued.py
@@ -0,0 +1,32 @@
+
+
+
+def squareit(num):
+    """ return square of number"""
+
+    return num * num
+
+
+def cubeit(num):
+    """ return cube of a number"""
+
+    return num * num * num 
+
+def main():
+    
+    anum = int(input("Please enter a number"))
+    print(squareit(anum))
+    print(cubeit(anum))
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
